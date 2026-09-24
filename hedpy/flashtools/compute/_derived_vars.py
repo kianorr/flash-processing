961770c213f1062b8c337f7c46ce6d1d020af710
kianorr <ko9821@princeton.edu>
2026-09-24 15:19:48 -0400

    update files with files from main

--- hedpy/flashtools/compute/_derived_vars.py
+++ hedpy/flashtools/compute/_derived_vars.py
@@ -1,6 +1,8 @@
 import scipy
 import numpy as np
 from copy import deepcopy
+
+from scipy import constants
 from .data_index import register_compute_func, data_index
 from hedpy.flashtools.utils import celsius_to_eV
 
@@ -177,14 +179,21 @@ def kinetic_energy(data, data_yt, **kwargs):
     name="B_r",
     label=r"$B_{r}$",
     description="Corrected Br",
-    units="kG",
+    units="G",
     cmap="RdBu",
     data_deps=["magx"],
     divergent=True,
     plot_log10=False,
 )
 def B_r(data, data_yt, **kwargs):
-    data["B_r"] = {"data": data["magx"]["data"] * np.sqrt(4 * np.pi) * 1e-3}
+    if data["magx"]["data"].squeeze().ndim == 3:
+        r = np.sqrt(data["x"]**2 + data["y"]**2)
+        r_safe = np.where(r == 0, 1e-12, r)
+
+        B_r   = (data["B_x"] * data["x"] + data["B_y"] * data["y"]) / r_safe
+    else:
+        B_r   = data["magx"]["data"] * np.sqrt(4 * np.pi)
+    data["B_r"] = {"data": B_r}
     return data
 
 
@@ -425,6 +434,113 @@ def mag_planar(data, data_yt, **kwargs):
 
 
 @register_compute_func(
+    name="|mag|",
+    label="$\\sqrt{B_x^2 + B_y^2 + B_z^2}$",
+    units="G / $\sqrt{4 \pi}$",
+    data_deps=["magx", "magy", "magz"],
+    cmap="plasma",
+    # data_plot_lims=[0, 3500],
+    plot_log10=False,
+)
+def mag_magnitude(data, data_yt, **kwargs):
+    bx = data["magx"]["data"]
+    by = data["magy"]["data"]
+    bz = data["magz"]["data"]
+    data["|mag|"] = {"data": np.sqrt(bx**2 + by**2 + bz**2)}
+    return data
+
+
+@register_compute_func(
+    name="grad_nele",
+    label="$\\nabla n_e$",
+    units="cm$^{-4}$",
+    data_deps=["nele", "first_coord", "second_coord", "third_coord"],
+    cmap="plasma",
+    # data_plot_lims=[0, 3500],
+    plot_log10=False,
+)
+def grad_nele(data, data_yt, **kwargs):
+    nele = data["nele"]["data"]
+    grad_nele = np.gradient(
+        nele, 
+        data["first_coord"]["data"], 
+        data["second_coord"]["data"], 
+        data["third_coord"]["data"]
+    )
+    data["grad_nele"] = {"data": grad_nele}
+    return data
+
+
+@register_compute_func(
+    name="grad_T_e",
+    label="$\\nabla T_e$",
+    units="eV/cm",
+    data_deps=["T_e", "first_coord", "second_coord", "third_coord"],
+    cmap="plasma",
+    # data_plot_lims=[0, 3500],
+    plot_log10=False,
+)
+def grad_T_e(data, data_yt, **kwargs):
+    T_e = data["T_e"]["data"]
+    grad_T_e = np.gradient(
+        T_e, 
+        data["first_coord"]["data"], 
+        data["second_coord"]["data"], 
+        data["third_coord"]["data"]
+    )
+    data["grad_T_e"] = {"data": grad_T_e}
+    return data
+
+
+@register_compute_func(
+    name="dB/dt_bier",
+    label="$\\frac{dB}{dt}$ (Biermann)",
+    units="G / $\sqrt{4 \pi}$",
+    data_deps=["grad_T_e", "grad_nele", "nele"],
+    cmap="plasma",
+    # data_plot_lims=[0, 3500],
+    plot_log10=False,
+)
+def dB_dt_bier(data, data_yt, **kwargs):
+    grad_T_e = data["grad_T_e"]["data"] * np.full(3, 1e-2)[:, None, None, None]
+    grad_nele = data["grad_nele"]["data"] * np.full(3, 1e-8)[:, None, None, None]
+    nele = data["nele"]["data"] * 1e-6
+    data["dB/dt_bier"] = {"data": grad_nele * grad_T_e / (nele * constants.elementary_charge)}
+    return data
+
+
+@register_compute_func(
+    name="resistivity",
+    label="$\\eta$",
+    units="$\Omega \cdot$ cm",
+    data_deps=["Z_bar", "T_e"],
+    cmap="plasma",
+    # data_plot_lims=[0, 3500],
+    plot_log10=False,
+)
+def resistivity(data, data_yt, **kwargs):
+    coulomb_log = 5.0
+    data["resistivity"] = {"data": 1.03e-2 * data["Z_bar"]["data"] * coulomb_log * data["T_e"]["data"] ** (-3/2)}
+    return data
+
+
+@register_compute_func(
+    name="Rm",
+    label="$Rm$",
+    units="~",
+    data_deps=["vel_mag", "resistivity"],
+    cmap="plasma",
+    # data_plot_lims=[0, 3500],
+    plot_log10=False,
+)
+def Rm(data, data_yt, **kwargs):
+    length_scale = kwargs.get("length_scale", 0.1) * 1e-2 # input in cm, convert to m
+    Rm = constants.mu_0 * (data["vel_mag"]["data"] * 1e-2) * length_scale / (data["resistivity"]["data"] * 1e-2)
+    data["Rm"] = {"data": Rm}
+    return data
+
+
+@register_compute_func(
     name="|mag_xy|",
     label="$\\sqrt{B_x^2 + B_y^2}$",
     units="G",
@@ -539,12 +655,14 @@ def helicity_density(data, data_yt, **kwargs):
     name="helicity",
     label=r"$H$",
     units="cm$^3$/s",
-    data_deps=["helicity_density", "first_coord", "second_coord", "third_coord"],
+    data_deps=["helicity_density", "first_coord", "second_coord", "third_coord", "targ"],
     cmap="plasma",
     plot_log10=False,
 )
 def helicity(data, data_yt, **kwargs):
-    u_dot_w = data["helicity_density"]["data"]
+    u_dot_w = data["helicity_density"]["data"].copy()
+    if kwargs["multiply_by_targ"]:
+        u_dot_w *= data["targ"]["data"]
     
     dx = data["first_coord"]["data"][1] - data["first_coord"]["data"][0]
     dy = data["second_coord"]["data"][1] - data["second_coord"]["data"][0]
